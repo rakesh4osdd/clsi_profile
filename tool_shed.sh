d28af6240e529c37439ca2c6949a172a3b5fcff1
Rakesh Kumar <rakesh4osdd@gmail.com>
2021-06-11 18:36:04 +0530

    Toolshed settings

---
+++ tool_shed.sh
@@ -0,0 +1,7 @@
+planemo shed_init --name="clsi_profile" \
+                  --owner="rakesh4osdd" \
+                  --description="CLSI profile using MIC breakpoints" \
+                  --remote_repository_url="https://github.com/rakesh4osdd/clsi_profile/tree/master" \
+                  --homepage_url="https://github.com/rakesh4osdd/clsi_profile/tree/master" \
+                  --long_description="CLSI profile using MIC breakpoints" \
+                 # --category='' 
