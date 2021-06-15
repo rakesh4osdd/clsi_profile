270954085d85cc8a8e38173221368acdabc57a38
Rakesh Kumar <rakesh4osdd@gmail.com>
2021-06-16 00:03:53 +0530

    planemo script for asist.xml template

--- tool_asist.sh
+++ tool_asist.sh
@@ -3,12 +3,11 @@
 planemo tool_init --id 'asist' --name 'ASIST' --version 1.0.1 \
           --description 'Antimicrobial Susceptibility standards based phenotypes' \
           --requirement 'pandas' \
-          --example_command "asist_dynamic.py 'asist_input.csv'  'asist_output.csv'" \
+          --example_command "\"${__tool_directory__}/asist_dynamic.py\" 'asist_input.csv'  'asist_output.csv'" \
           --example_input test-data/asist_input.csv \
-          --example_output asist_output.csv \
+          --example_output test-data/asist_output.csv \
           --test_case \
 	  --version_command 'python -c "import pandas; print(pandas.__version__)"' \
-#          --help_from_command '' \
           --doi 'https://ab-openlab.csir.res.in/asist'
 
-planemo lint
+planemo lint asist.xml
