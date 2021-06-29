d5ccc121a7aa9152a3cfc4a19982d320a5a403e1
Rakesh Kumar <rakesh4osdd@gmail.com>
2021-06-29 16:26:46 +0530

    test tool script:

--- tool.sh
+++ tool.sh
@@ -4,9 +4,9 @@ planemo tool_init --id 'clsi_profile' --name 'CLSI Profile' \
           --description 'MIC profile using CLSI MIC breakpoints' \
           --requirement 'python' \
           --example_command "clsi_profile_type2.py 'input.csv' 'clsi.csv' 'output.csv'" \
-          --example_input input.csv \
-	  --example_input clsi.csv \
-          --example_output output.csv \
+          --example_input test-data/input.csv \
+	  --example_input test-data/clsi.csv \
+          --example_output test-data/output.csv \
           --test_case \
 #          --version_command 'python3 -V' \
 #          --help_from_command '' \
