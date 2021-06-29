3dc5531e1f73063315a22a15c71c8b901d60e6ae
Rakesh Kumar <rakesh4osdd@gmail.com>
2021-06-29 16:24:35 +0530

    remove test scripts

--- tool_shed.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# After reviewing .shed.yml, this configuration file and relevant shed artifacts can be quickly linted using the following command.
-planemo shed_lint --tools
-planemo shed_create --shed_target toolshed 
